3d08bbf6532a461017ee459d18efc112e73f9d66
Mohamed Ghoneim <maghoneim@2p.com.sa>
2023-08-29 08:45:20 +0300

    isolate

--- lib/network_implementation.dart
+++ lib/network_implementation.dart
@@ -1,6 +1,7 @@
 library network_implementation;
 export 'package:network_implementation/src/http_method.dart';
 export 'package:network_implementation/src/network.dart';
+export 'package:network_implementation/src/isolate_thread.dart';
 export 'package:network_implementation/src/network_implementation.dart';
 
 /// A Calculator.
